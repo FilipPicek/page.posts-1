9bc5cde609524fed3b3bc481cb1edf1849baef15
FilipPicek <136388808+FilipPicek@users.noreply.github.com>
2024-01-04 00:03:56 +0100

    1

---
+++ index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+
+
+
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.static("public"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+app.get("/", (req, res) => {
+  
+    
+    res.render("index.ejs");
+  });
+
+app.get("/submit", (req, res) => {
+ 
+  res.render("index.ejs");
+  
+  });
+
+  app.get("/post", (req, res) => {
+    // Ovdje koristimo metodu `res.sendFile` da pošaljemo sadržaj iz 'posts.ejs'
+    res.render("posts.ejs");
+
+  });
+
+
+
+  
+  
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
